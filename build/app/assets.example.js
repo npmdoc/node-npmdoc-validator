cd923b830c4388ea42b1a1abd633cbdff55bb2d1
Travis CI User <travis@example.org>
2017-04-18 19:06:21 +0000

    [ci skip] update gh-pages

--- build/app/assets.example.js
+++ build/app/assets.example.js
@@ -291,14 +291,15 @@ utility2-comment -->\n\
         local.assetsDict['/assets.example.js'] =
             local.assetsDict['/assets.example.js'] ||
             local.fs.readFileSync(__filename, 'utf8');
+        // bug-workaround - long $npm_package_buildCustomOrg
+        /* jslint-ignore-begin */
         local.assetsDict['/assets.npmdoc_validator.rollup.js'] =
             local.assetsDict['/assets.npmdoc_validator.rollup.js'] ||
             local.fs.readFileSync(
-                // buildCustomOrg-hack
-                local.npmdoc_validator.__dirname +
-                    '/lib.npmdoc_validator.js',
+                local.npmdoc_validator.__dirname + '/lib.npmdoc_validator.js',
                 'utf8'
             ).replace((/^#!/), '//');
+        /* jslint-ignore-end */
         local.assetsDict['/favicon.ico'] = local.assetsDict['/favicon.ico'] || '';
         // if $npm_config_timeout_exit exists,
         // then exit this process after $npm_config_timeout_exit ms
